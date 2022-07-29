8a5104ab582e2cd40945c85f01e0f4be442a9cf2
Tiago Guatierri <tiagovit@gmail.com>
2022-07-29 17:30:55 -0300

    test:add customer

--- src/app/modules/customer/components/add-customer/add-customer.component.spec.ts
+++ src/app/modules/customer/components/add-customer/add-customer.component.spec.ts
@@ -1,34 +1,122 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from './../../../../material.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from './../../../../core/services/api.service';
+import {
+  createRoutingFactory,
+  SpectatorRouting,
+  SpyObject,
+} from '@ngneat/spectator';
 import { AddCustomerComponent } from './add-customer.component';
-import { ApiService } from '../../../../core/services/api.service';
-
-import { render, screen } from '@testing-library/angular';
-
-import userEvent from '@testing-library/user-event';
-
-const getSubmitButton = () => screen.getByTestId('submit') as HTMLButtonElement;
-const getDeleteButton = () => screen.getByTestId('delete') as HTMLButtonElement;
-const getInputName = () => screen.getByTestId('inputName') as HTMLInputElement;
+import { ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 describe('AddCustomerComponent', () => {
-  const apiServiceSpy = jasmine.createSpyObj<ApiService>('ApiService', [
-    'post',
-    'put',
-    'delete',
-    'get',
+  let component: AddCustomerComponent;
+  let fixture: ComponentFixture<AddCustomerComponent>;
+  let spectator: SpectatorRouting<AddCustomerComponent>;
+
+  const apiServiceSpy: SpyObject<ApiService> = jasmine.createSpyObj(
+    'ApiService',
+    ['post', 'put', 'delete', 'get']
+  );
+
+  const routerSpy: SpyObject<Router> = jasmine.createSpyObj('Router', [
+    'navigateByUrl',
   ]);
 
-  beforeEach(async () => {
-    await render(AddCustomerComponent, {
-      providers: [{ provide: ApiService, useValue: apiServiceSpy }],
-      imports: [FormsModule, ReactiveFormsModule],
+  const createComponent = createRoutingFactory({
+    component: AddCustomerComponent,
+    imports: [MaterialModule, HttpClientTestingModule, RouterTestingModule],
+    providers: [
+      {
+        provider: ApiService,
+        useValue: apiServiceSpy,
+      },
+      {
+        provider: Router,
+        useValue: routerSpy,
+      },
+    ],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+    fixture = spectator.fixture;
+  });
+
+  describe('whitout route params', () => {
+    it('only mount component', () => {
+      expect(component).toBeTruthy();
     });
   });
 
-  it('when user submit form', () => {
-    userEvent.type(getInputName(), 'any_text');
-    userEvent.click(getSubmitButton());
+  describe('when has id on route params', () => {
+    it('should call api to get customer by id and patch customer form', fakeAsync(() => {
+      // need for ngOnInit
+      spectator.detectChanges();
+      spectator.setRouteParam('id', '123');
 
-    expect(apiServiceSpy.post).toHaveBeenCalledTimes(1)
-  })
+      // await observable resolve
+      tick();
+
+      apiServiceSpy.get.and.returnValue({});
+
+      spyOn(component, 'patchCustomer').and.callThrough();
+      apiServiceSpy.get({});
+
+      component.patchCustomer({
+        fullName: 'teste',
+      });
+
+      expect(apiServiceSpy.get).toHaveBeenCalled();
+      expect(component.patchCustomer).toHaveBeenCalled();
+      expect(component.form.get('fullName')).not.toBeEmpty();
+    }));
+
+    it('expect delete button is enabled', fakeAsync(() => {
+      // need for ngOnInit
+      spectator.detectChanges();
+
+      // await observable resolve
+      tick();
+
+      component.customerId = '123';
+      // to update button state
+      spectator.detectChanges();
+
+      const deleteButton = spectator.query<HTMLButtonElement>('#deleteButton')!;
+
+      expect(deleteButton.disabled).toBeFalse();
+    }));
+  });
+
+  describe('when submit a form', () => {
+    it('if customeId has value defined, shuold update customer', () => {
+      component.customerId = '123';
+
+      apiServiceSpy.put({});
+      routerSpy.navigateByUrl('');
+
+      spyOn(component, 'onSubmit').and.callThrough();
+
+      component.onSubmit();
+
+      expect(apiServiceSpy.put).toHaveBeenCalled();
+      expect(routerSpy.navigateByUrl).toHaveBeenCalled();
+    });
+
+    it('if customeId is empty, shuold create customer', () => {
+      apiServiceSpy.post({});
+      routerSpy.navigateByUrl('');
+
+      spyOn(component, 'onSubmit').and.callThrough();
+
+      component.onSubmit();
+
+      expect(apiServiceSpy.post).toHaveBeenCalled();
+      expect(routerSpy.navigateByUrl).toHaveBeenCalled();
+    });
+  });
 });
